37dbba339cf0a617c882cad4421f6b85eb17787a
N7581 <N7581@student.jamk.fi>
2020-05-16 18:59:13 +0300

    Testing

--- buzz/generator.py
+++ buzz/generator.py
@@ -6,7 +6,7 @@ buzz = ('continuous testing', 'continuous integration',
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+verbs = ('p_accelerates', 'p_improves', 'p_enhances', 'p_revamps', 'p_boosts')
 
 def sample(l, n = 1):
     result = random.sample(l, n)
